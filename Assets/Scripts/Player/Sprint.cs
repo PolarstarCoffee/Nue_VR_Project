e54ade4606d6c3690e6e29c21040073e63d974ec
PolarstarCoffee <98419830+PolarstarCoffee@users.noreply.github.com>
2022-05-18 01:20:52 -0500

    5/18/2022 Push
    small update on sprint script

--- Assets/Scripts/Player/Sprint.cs
+++ Assets/Scripts/Player/Sprint.cs
@@ -1,12 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.XR.CoreUtils;
+using UnityEngine.XR.Interaction;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Controls;
+using UnityEngine.XR.Interaction.Toolkit.Inputs;
+
 
 public class Sprint : MonoBehaviour
 {
+
+    private XROrigin _XROriginRig;
+    public float speed = 3.0f;
+    [SerializeField] private InputActionReference SprintButton;
+
+
     // Start is called before the first frame update
     void Start()
     {
+     _XROriginRig = GetComponent<XROrigin>();
         
     }
 
@@ -14,5 +27,10 @@ public class Sprint : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void Run()
+    {
+        if (!)
     }
 }
