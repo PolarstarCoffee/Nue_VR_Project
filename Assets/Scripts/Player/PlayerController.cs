076b41597f4787ddeae7c90bc91f575badfbe50c
PolarstarCoffee <98419830+PolarstarCoffee@users.noreply.github.com>
2022-05-19 22:08:03 -0500

    5/19/2022 push
    Included:
    
    -Edited Player Controller to include a dash button, set to the position of the camera.

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -13,28 +13,36 @@ public class PlayerController : MonoBehaviour
 
     [SerializeField] private InputActionReference jumpActionReference;
     [SerializeField] private float jumpForce = 500.0f;
-    
+    [SerializeField] private InputActionReference DashReference;
+
     private Rigidbody _body;
     private XROrigin _xROrigin;
     private CapsuleCollider _collider;
-
+    private Camera _camera;
 
     private bool IsGrounded => Physics.Raycast(new Vector2(transform.position.x, transform.position.y + 3.0f), Vector3.down, 3.0f); //Raycast checking if player is grounded (Originally 2.0f)
     // Start is called before the first frame update
     void Start()
     {
-        _xROrigin = GetComponent<XROrigin>();
-        _collider = GetComponent<CapsuleCollider>();
+        _xROrigin = GetComponent<XROrigin>(); //grabs player rig
+        _collider = GetComponent<CapsuleCollider>(); //Grabs player capsule collider
         _body = GetComponent<Rigidbody>(); //grabs players rigid body
+        _camera = GetComponent<Camera>(); //Grabs player camera
+
         jumpActionReference.action.performed += OnJump; //Specifies whenever the action is performed, this function would execute
+
+        DashReference.action.performed += OnDash;
     }
 
     // Update is called once per frame
     void Update()
     {
+       
         var center = _xROrigin.CameraInOriginSpacePos;
         _collider.height = Mathf.Clamp(_xROrigin.CameraInOriginSpaceHeight, 1.0f, 3.0f);
         _collider.center = new Vector3(center.x, _collider.height / 2, center.z);
+        var camDir =  Camera.main.transform.forward;
+        Vector3 dir = camDir.normalized;
     }
     private void OnJump(InputAction.CallbackContext obj)
     {
@@ -45,5 +53,15 @@ public class PlayerController : MonoBehaviour
         } //If the player is not grounded, the player cannot jump
         _body.AddForce(Vector3.up * jumpForce); //otherwise, add force to the player going upwards, dictated by the jump force
 
+    }
+
+    private void OnDash(InputAction.CallbackContext obj)
+    {
+        Vector3 camForward = Camera.main.transform.forward;
+        if (!IsGrounded)
+        {
+            return;
+        }
+        _body.AddForce(camForward * jumpForce);
     }
 }
--- Assets/Scripts/Player/Sprint.cs
+++ Assets/Scripts/Player/Sprint.cs
@@ -12,15 +12,19 @@ public class Sprint : MonoBehaviour
 {
 
     private XROrigin _XROriginRig;
-    public float speed = 3.0f;
-    [SerializeField] private InputActionReference SprintButton;
+    public float speed = 10.0f;
+    [SerializeField] private InputActionReference SprintButtonReference;
+    private Rigidbody _body;
+
+
 
 
     // Start is called before the first frame update
     void Start()
     {
-     _XROriginRig = GetComponent<XROrigin>();
-        
+     _XROriginRig = GetComponent<XROrigin>(); //Grabs player rig
+        _body = GetComponent<Rigidbody>(); //grabs players rigid body
+        SprintButtonReference.action.performed += Run; //Specifies whenevr the action is performed, this function would execute
     }
 
     // Update is called once per frame
@@ -29,8 +33,9 @@ public class Sprint : MonoBehaviour
         
     }
 
-    private void Run()
+    private void Run(InputAction.CallbackContext obj)
     {
-        if (!)
+      
+
     }
 }
